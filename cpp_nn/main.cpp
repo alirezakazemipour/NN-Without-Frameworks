9d5e6c244b725a6e5ef8a64e227c98bc18a59912
Alireza Kazemipour <kazemipour.alireza@gmail.com>
2021-11-18 11:44:30 +0330

    [cpp_nn] Remove train_regression & train_classification files and instead, merge them into single main.cpp for performing better CI Ops.

--- cpp_nn/main.cpp
+++ cpp_nn/main.cpp
@@ -42,8 +42,18 @@ public:
     }
 };
 
+void train_regression();
+void train_classification();
+
 int main()
 {
+    train_regression();
+    train_classification();
+    return 0;
+}
+
+void train_classification(){
+    cout <<"-----Classification-------"<<endl;
     int num_samples = 500;
     int num_features = 2;
     int num_classes = 3;
@@ -117,8 +127,8 @@ int main()
         } else {
             smoothed_loss = (float) (0.9 * smoothed_loss + 0.1 * total_loss);
         }
-
-        cout<<"Step: " << step <<" | loss: " << smoothed_loss<<endl;
+        if (step % 100 ==0)
+            cout<<"Step: " << step <<" | loss: " << smoothed_loss<<endl;
     }
     y = my_net.forward(x);
     int predicted_class[batch_size];
@@ -140,6 +150,60 @@ int main()
         }
     }
     cout<<"training acc: " << float(true_positives) / float(batch_size) << endl;
-    return 0;
+}
 
+void train_regression(){
+    cout <<"-----Regression-------"<<endl;
+
+    float_batch x(200, vector<float>(1, 1));
+    float_batch t(200, vector<float>(1, 1));
+
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> d{0, 0.1};
+
+
+    for(int i = -100; i < 100; i++){
+        //        cout<<i<<endl;
+        x[i + 100][0] = 0.01 * i;
+        t[i + 100][0] = pow(x[i + 100][0], 2) + d(gen);
+
+    }
+    MyNet my_net = MyNet{1, 1};
+    MSELoss mse{};
+    //    Momentum opt(0.2, 0.8, my_net.parameters);
+    //    SGD opt(0.3, my_net.parameters);
+    //    RMSProp opt(0.01, 0.99, my_net.parameters);
+    //    AdaGrad opt(0.05, my_net.parameters);
+    Adam opt(0.003, 0.9, 0.999, my_net.parameters);
+    float_batch y;
+
+    for(int epoch = 0; epoch < 1000; epoch++){
+        y= my_net.forward(x);
+        Loss loss = mse.apply(y, t);
+        my_net.backward(loss);
+        opt.apply();
+        if (epoch % 100 == 0)
+            cout<<"Step: " << epoch <<" | loss: " << loss.value<<endl;
+    }
+
+
+    //    for(size_t i = 0; i < x.size(); i++){
+    //        cout<<"x = " << x[i][0]<<endl;
+    //        cout<<"t = " << t[i][0]<<endl;
+    //        cout<<"y = " << y[i][0]<<endl;
+    //        cout<< "delta = " << loss.delta[i][0]<<endl;
+    //    }
+
+    //    Utils utils;
+    //    float_batch a = {{1, 2}, {3, 4}};
+    //    float_batch b = {{5, 6}, {7, 8}};
+    //    float_batch c = utils.mat_mul(a, b);
+
+    //    for(int i = 0; i < 2; i++){
+    //        for(int j = 0; j < 2; j++){
+    //            cout<< a[i][j]<<endl;
+
+    //            }
+    //        }
 }
